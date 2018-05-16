fbb84b9b5dadd720f8e254b82c51006aaddb28b5
zhiyong wang <zhiyong.wang1@corp.elong.com>
2018-05-16 17:45:20 +0800

    complete

--- bin/myblog.js
+++ bin/myblog.js
@@ -12,23 +12,17 @@ program.command('help').description('显示使用帮助').action(function() {
     program.outputHelp();
 });
 
-
 // create 命令
-program.command('create [dir]').description('创建一个空的博客').action(function() {
-    console.log('create %s', dir);
-});
+program.command('create [dir]').description('创建一个空的博客').action(require('../lib/cmd_create'));
 
 // preview 命令
 program.command('preview [dir]').description('实时预览').action(require('../lib/cmd_preview'));
 
-
 // build 命令
-
-program.command('build [dir]').description('生成整站静态html').option('-o, --output <dir>', '生成的静态html存放目录').action(function() {
-    console.log('preview %s', dir);
-}).action(function(dir, options) {
-    console.log('create %s, output %s', dir, options.output);
-});
+program.command('build [dir]').
+description('生成整站静态html').
+option('-o, --output <dir>', '生成的静态html存放目录').
+action(require('../lib/cmd_build'));
 
 // 开始解析
 
